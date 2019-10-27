2b4db1521c109a62a94a7337837b59066cec9209
Gilbert <Marie.Gilbert@sony.com>
2019-10-26 21:59:05 -0700

    added current date

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
+$(document).ready(function(){
 
-
-//.val versus .text//
-//var userText = $("#userinput").text().trim();
+//Display Date
+var currentDayText = moment().format("MMM Do YY");
+$("#currentDay").text("City Name " + "(" + (currentDayText) +")");
 
 $(".search").on("click", function(){
     var userInput = $("#searchCity").val().trim();
-    //console.log(userInput);
-   // console.log(userText);
 
    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial&appid=1fede5b00042eb37df138bc327a6d612";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-//go to this url and get the info, then run the function//
-}).then(function (response){
-//console.log(response) use the response object elements to build out the info in html 
-console.log(response);
-console.log(response.name);
-console.log("Temp: " + response.main.temp); 
-console.log("Humidity: " + response.main.humidity);
-console.log("Wind Speed: " + response.wind.speed);
-console.log(moment().format("MMM Do YY"));
-//make a jquery element to display the value of the text element 
-})
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    //go to this url and get the info, then run the function//
+    }).then(function (response){
+        //console.log(response) use the response object elements to build out the info in html 
+
+  //call function to fill descriptions
+  displayStorage();
+
+        //make a jquery element to display the value of the text element 
+        var cityName = localStorage.getItem($("<#li>").val(response.name));
+        var cityTemp = response.main.temp;
+        var cityHumidity = response.main.humidity;
+        var windSpeed = response.wind.speed;
+
+
+
+        //Set "response values" key in localStorage
+        localStorage.setItem("city", response.name);
+        localStorage.setItem("temp", response.main.temp);
+        localStorage.setItem("humidity", response.main.humidity);
+        localStorage.setItem("wind", response.wind.speed);
+        localStorage.setItem("current", moment().format("MMM Do YY"));
+    })
 })
 
-//still need to add * UV index and Icon image (visual representation of weather conditions)//
+function displayStorage() {
+    var city = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    for (var i = 0; i < cities.length; i++) {
+      var response = localStorage.getItem(city[i]);
+      $("#" + city[i]).find("textarea").val(response);
+    }
+  }
+});
